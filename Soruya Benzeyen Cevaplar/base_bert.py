f3b53cde8452acae8fdc03749f5e8cb5644ba141
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2024-03-19 11:31:05 +0300

    Model info

--- Soruya Benzeyen Cevaplar/base_bert.py
+++ Soruya Benzeyen Cevaplar/base_bert.py
@@ -66,5 +66,6 @@ for _, row in sample_questions.iterrows():
     if row.name in sorted_similarities.index[:5]:
         top5_success += 1
 
+print("Base BERT")
 print(f"Top1 Başarısı: {top1_success / 1000}")
 print(f"Top5 Başarısı: {top5_success / 1000}")
--- Soruya Benzeyen Cevaplar/medium_bert.py
+++ Soruya Benzeyen Cevaplar/medium_bert.py
@@ -4,6 +4,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 from transformers import AutoTokenizer, AutoModel
 import torch
 
+N = 1
+
 # Hugging Face BERT modelini yükle
 tokenizer = AutoTokenizer.from_pretrained(
     "ytu-ce-cosmos/turkish-medium-bert-uncased")
@@ -13,7 +15,9 @@ model = AutoModel.from_pretrained("ytu-ce-cosmos/turkish-medium-bert-uncased")
 if torch.cuda.is_available():
     model = model.cuda()
 
+
 def get_vector(text):
+    global N
     # Metni modele göre tokenize et ve vektör temsilini al
     inputs = tokenizer(text, return_tensors="pt",
                        padding=True, truncation=True, max_length=512)
@@ -23,7 +27,10 @@ def get_vector(text):
     with torch.no_grad():
         outputs = model(**inputs)
     # Sonuçları CPU'ya geri taşı ve numpy array'ine çevir
+    print(f"{N}/103.126")
+    N += 1
     return outputs.last_hidden_state.mean(dim=1).cpu().numpy()
+
 
 # Veri setini yükle
 df = pd.read_csv('../dataset/instructions.csv', index_col=0)
@@ -37,8 +44,10 @@ df['soru_vektor'] = df['soru'].apply(lambda x: get_vector(x))
 df['cevap_vektor'] = df['çıktı'].apply(lambda x: get_vector(x))
 
 # Rastgele 1000 soru seç
-sample_questions = df.sample(n=1000, random_state=42)
+sample_questions = df.iloc[:1000]
+# df.sample(n=1000, random_state=42)
 
+M = 1
 top1_success = 0
 top5_success = 0
 
@@ -48,11 +57,15 @@ for _, row in sample_questions.iterrows():
         question_vector.reshape(1, -1), x['cevap_vektor'].reshape(1, -1))[0][0], axis=1)
     sorted_similarities = similarities.sort_values(ascending=False)
 
+    print(f"{M}/1000")
+    M += 1
+
     # Top1 ve Top5 başarıyı kontrol et
     if row.name == sorted_similarities.index[0]:
         top1_success += 1
     if row.name in sorted_similarities.index[:5]:
         top5_success += 1
 
+print("Medium BERT")
 print(f"Top1 Başarısı: {top1_success / 1000}")
 print(f"Top5 Başarısı: {top5_success / 1000}")
